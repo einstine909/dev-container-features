b77b7293a0bf11817b6125f2d19cc81db047ecfb
Seth Pierson <einstine909@gmail.com>
2026-08-09 09:26:15 -0400

    trying to mount the opencode configs a different way

--- src/opencode/install.sh
+++ src/opencode/install.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
 
 # Make autostart script
-OPENCODE_AUTOSTARTER="/opt/opencode-autostart"
-echo "#!/bin/sh" > "$OPENCODE_AUTOSTARTER"
+OPENCODE_INIT="/opt/opencode-init"
+echo "#!/bin/sh" > "$OPENCODE_INIT"
+
+echo "mount --bind -o ro /mnt/config/opencode /home/$_REMOTE_USER/.config/opencode"
+
 if [ "$AUTOSTART" = "true" ]; then
-    echo -n "nohup opencode " >> "$OPENCODE_AUTOSTARTER"
+    echo -n "nohup opencode " >> "$OPENCODE_INIT"
 
 	if [ -n "$OPENCODE_ARGS" ]; then
-		echo -n "${OPENCODE_ARGS}" >> "$OPENCODE_AUTOSTARTER"
+		echo -n "${OPENCODE_ARGS}" >> "$OPENCODE_INIT"
 	fi
-	echo " > /var/logs/opencode.log 2>&1 &" >> "$OPENCODE_AUTOSTARTER"
+	echo " > /var/logs/opencode.log 2>&1 &" >> "$OPENCODE_INIT"
 
 fi
 
-chmod +x "$OPENCODE_AUTOSTARTER"
+chmod +x "$OPENCODE_INIT"
 
 # Install opencode
 su - "$_REMOTE_USER" -c "curl -fsSL https://opencode.ai/install | bash"
